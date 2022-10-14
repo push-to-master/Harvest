0fd4bb9b0debb29f71c9e11c42627edf79c27d8e
ULTRAWHIZKID <65918215+MJ-Loo@users.noreply.github.com>
2022-10-14 16:58:45 +0200

    testing files

--- frontend/src/components/test/logs-table.test.js
+++ frontend/src/components/test/logs-table.test.js
@@ -9,3 +9,41 @@ test('Renders table with headings', () => {
   const tableheader = screen.getByText(/date/i);
   expect(tableheader).toBeInTheDocument();
 });
+test('Renders table with Farmer', () => {
+  render(<Router>
+    <LogsTable/>
+  </Router>);
+  const tableheader = screen.getByText(/farmer/i);
+  expect(tableheader).toBeInTheDocument();
+});
+test('Renders table with Category', () => {
+  render(<Router>
+    <LogsTable/>
+  </Router>);
+  const tableheader = screen.getByText(/category/i);
+  expect(tableheader).toBeInTheDocument();
+});
+
+test('Renders table with Croptype', () => {
+  render(<Router>
+    <LogsTable/>
+  </Router>);
+  const tableheader = screen.getByText(/crop/i);
+  expect(tableheader).toBeInTheDocument();
+});
+
+test('Renders table with produce type', () => {
+  render(<Router>
+    <LogsTable/>
+  </Router>);
+  const tableheader = screen.getByText(/produce/i);
+  expect(tableheader).toBeInTheDocument();
+});
+
+test('Renders table with description', () => {
+  render(<Router>
+    <LogsTable/>
+  </Router>);
+  const tableheader = screen.getByText(/description/i);
+  expect(tableheader).toBeInTheDocument();
+});
--- frontend/src/components/test/orgs-card.test.js
+++ frontend/src/components/test/orgs-card.test.js
@@ -2,10 +2,19 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import OrgsCard from '../orgs-card'
 
-test('Renders table with headings', () => {
+test('Renders card and checks for analytics', () => {
   render(<Router>
     <OrgsCard/>
   </Router>);
   const tableheader = screen.getByText(/view analytics/i);
+  expect(tableheader).toBeInTheDocument();
+});
+
+
+test('Renders card and checks for logs', () => {
+  render(<Router>
+    <OrgsCard/>
+  </Router>);
+  const tableheader = screen.getByText(/logs/i);
   expect(tableheader).toBeInTheDocument();
 });
